f51f367bc51695feb1a3931b032041ab315aa88c
Omar El Houmadi <oel-houm@e1r4p3.1337.ma>
2023-04-28 18:52:40 +0100

    work 4

--- minishell.c
+++ minishell.c
@@ -3,20 +3,130 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wbouwach <wbouwach@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oel-houm <oel-houm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 22:00:56 by wbouwach          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/04/28 15:16:32 by oel-houm         ###   ########.fr       */
-=======
-/*   Updated: 2023/04/27 17:58:47 by wbouwach         ###   ########.fr       */
->>>>>>> 06dea568cd8f3cf4eb7dfa051c3e5c6b13304899
+/*   Updated: 2023/04/28 17:50:53 by oel-houm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "minishell.h"
 
 int global_exit;
+
+
+
+
+
+static int	number_of_args(char **args)
+{
+	int	count;
+
+	count = 0;
+	while (args[count] != NULL)
+	{
+		//printf("args[%d]: %s\n", count, args[count]);
+		count++;
+	}
+	//printf("count:%d\n", count);
+	return (--count);
+}
+
+static int	ft_isnum(char *str) // ft_isnum ft_isdigit
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+void		exit_cmd(char **cmd) // exit_cmd takes 2 params: cmd + copy_of_env
+{
+	//
+	//
+	if (number_of_args(cmd) > 1)
+	{
+		//4-set exit status to 1
+		if (cmd[1] && ft_isnum(cmd[1]) != 1)
+		{
+			ft_putstr_fd("exit\n", 2);
+			ft_putstr_fd("minishell: exit: ", 2);
+            ft_putstr_fd(cmd[1], 2);
+            ft_putstr_fd(": numeric argument required\n", 2);
+            exit(255);
+		}
+		else if (cmd && ft_isnum(cmd[1]) == 1)
+		{
+            ft_putstr_fd("exit\n", 2);
+            ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+		}
+	}
+	else if (number_of_args(cmd) == 1)
+	{
+		if (!(ft_isnum(cmd[1])))
+		{
+			ft_putstr_fd("exit\n", 2);
+			ft_putstr_fd("minishell: exit: ", 2);
+            ft_putstr_fd(cmd[1], 2);
+            ft_putstr_fd(": numeric argument required\n", 2);
+		}
+		int exit_value = ft_atoi(cmd[1]); //fix ft_atoi.c + change the value of $? in ENV
+        //printf("exit\n");
+		exit(exit_value);// check if cmd[1] is num then convert from char to int...
+		//execvp(cmd[0], cmd);
+	}
+	else if (number_of_args(cmd) == 0)
+	{
+		ft_putstr_fd("exit\n", 1);
+		exit(0);
+	}
+}
+
+void	pwd_cmd(char **line)
+{
+	(void)line;
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+		perror("getcwd"); //manage the error
+	//ft_putstr_fd(cwd, 1);
+	ft_putendl_fd(cwd, 1);
+	free(cwd);
+}
+
+
+
+
+// fix exit == 1
+void    parse_cmd(char **cmd) //,token
+{
+    if (ft_strncmp(cmd[0], "exit", 4) == 0 && !cmd[0][4])
+    {
+        exit_cmd(cmd);
+        int i = 0;
+        while (cmd[i])
+        {
+            free(cmd[i]);
+            i++;
+        }
+        free(cmd);
+    }
+    else if (ft_strncmp(cmd[0], "pwd" , 3) == 0 && !cmd[0][3])
+    {
+        pwd_cmd(cmd);
+    }
+}
+
 
 int main(int ac, char **av, char **env)
 {
@@ -38,36 +148,29 @@ int main(int ac, char **av, char **env)
             add_history(cmd);
         if (parse(cmd))
         {
-<<<<<<< HEAD
-            //int i = 0;
-            printf("%s\n", parse_operator(cmd));
-            free(cmd);
-            cmd = parse_operator(cmd);
-            //char **s = args_split(cmd);
-            //int *t = tokenise_cmd(s);
-            //while(s && s[i] && t && t[i])
-           // {
-             //   printf("%s ===== %d\n",s[i],t[i]);
-               // i++;
-            //}
-            
-=======
             int i = 0;
-            printf("%s\n", cmd);
-            
+            //printf("%s\n", parse_operator(cmd));
+            //free(cmd);
             cmd = parse_operator(cmd);
             char **s = args_split(cmd);
-            int *t = tokenise_cmd(s);
-            while (s[i] && t[i])
-            {
-                printf("%s === %d\n", s[i], t[i]);
-                i++;
-            }
-            //wgiuuggie
->>>>>>> 06dea568cd8f3cf4eb7dfa051c3e5c6b13304899
+            //int *t = tokenise_cmd(s);
+            // split cmd between pipes
+           // while(s && s[i])
+            //{
+                //parse_cmd -> parse_builtins
+                //printf("%s ===== %d\n",s[i],t[i]);
+                //int j = 0;
+                //while (s[j])
+                //{
+                 //   printf("%s\n", s[j]);
+                  //  j++;
+               // }
+               parse_cmd(s);
+               i++;
+            //}
         }
        // prompt(env_list);
-       free(cmd);
+       // free(cmd);
         cmd = readline(BLUE"MINISHELL$ "WHITE);
     }
     
